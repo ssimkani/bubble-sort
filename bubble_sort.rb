3bc86bb349f4c9875046645ecec8a94cd3d850e4
Seeba184 <seenasimkani@gmail.com>
2024-04-27 14:44:32 -0400

    feat: add loops to iterate through and sort array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,16 @@
 def bubble_sort
   out_of_order = true
   counter = 0
+
+  while out_of_order
+    for i in 0...(arr.length - 1)
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        counter += 1
+      end
+    end
+    out_of_order = false if counter == 0
+    counter = 0
+  end
+  arr
 end
